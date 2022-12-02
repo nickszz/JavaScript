961a5b2b88e12dfc65fbd1a7f74574a0e0face77
Nicolas Zaion Pereira Barbosa <117131044+nickszz@users.noreply.github.com>
2022-12-02 18:37:54 -0300

    JavaScript

---
+++ Exercicios/aula12ex/ex014/script.js
@@ -0,0 +1,17 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        //BOM DIA!
+        img.src = 'manha1.png'
+    } else if (hora >= 12 && hora < 18){
+        //BOA TARDE!
+        img.src = 'tarde1.png'
+    } else{
+        // BOA NOITE!
+        img.src = 'noite1.png'
+    }
+}
